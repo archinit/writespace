768017faa95691014c040777144b3c69848f1bb8
ArchitGitHubPro <archit.7537@gmail.com>
2025-06-05 19:07:00 +0530

    initial

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return <main>
+    <h1>hi from the main page</h1>  
+
+
+    <Button> click me</Button>
+  </main>
+}
