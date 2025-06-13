30ae7ee11cd178d0578c0b4aa284146f5bd31742
ArchitGitHubPro <archit.7537@gmail.com>
2025-06-13 17:30:48 +0530

    added clerk authentication

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import { Header } from "@/components/Header";
 
 
 
@@ -15,10 +17,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body>
+          <Header/>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
