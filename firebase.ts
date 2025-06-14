04e15988f6d471e97b70e6bb79d4c85fe27a740a
ArchitGitHubPro <archit.7537@gmail.com>
2025-06-14 12:32:47 +0530

    updated firebase.ts

--- firebase.ts
+++ firebase.ts
@@ -5,13 +5,13 @@ import { getFirestore } from "firebase/firestore";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCs4_RlVy3ENtxbgEfNEBodRIbNXEZ7-1o",
-  authDomain: "writespace-c8631.firebaseapp.com",
-  projectId: "writespace-c8631",
-  storageBucket: "writespace-c8631.firebasestorage.app",
-  messagingSenderId: "541777558148",
-  appId: "1:541777558148:web:91c971adb5b4b9e76360d1",
-  measurementId: "G-Z93VHVPL82"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0]; //check if app is created, give that or create a new one 
